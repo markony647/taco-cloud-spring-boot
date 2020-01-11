227786143e1276823fe67900fad6e28b2dbe6d19
Vitaly Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-01-11 13:43:47 +0200

    Initial commit for Taco Cloud project

---
+++ src/main/java/ua/marchenko/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package ua.marchenko.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ua/marchenko/tacocloud/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ua.marchenko.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
